2e1cda35c3faa3f284246126b9bdb585e583197e
Shumyk <dima.shumyk@gmail.com>
2020-05-26 14:51:45 +0300

    [351] Upgraded version of spring boot and gradle in order to thymeleaf accept Mono objects. adjusted controllers and templates to process calls.

--- src/main/java/com/shumyk/controllers/RecipeController.java
+++ src/main/java/com/shumyk/controllers/RecipeController.java
@@ -2,26 +2,28 @@ package com.shumyk.controllers;
 
 import com.shumyk.commands.RecipeCommand;
 import com.shumyk.services.RecipeService;
-import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class RecipeController {
 
     private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
 
-    private final RecipeService recipeService;
+    @NonNull private final RecipeService recipeService;
+    private WebDataBinder webDataBinder;
+
+    @InitBinder public void initBinder(final WebDataBinder webDataBinder) {
+        this.webDataBinder = webDataBinder;
+    }
 
     @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
@@ -39,21 +41,21 @@ public class RecipeController {
 
     @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
-        model.addAttribute("recipe", recipeService.findCommandById(id).block());
+        model.addAttribute("recipe", recipeService.findCommandById(id));
         return RECIPE_RECIPEFORM_URL;
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
-
+    public String saveOrUpdate(@ModelAttribute("recipe") RecipeCommand command){
+        webDataBinder.validate();
+        final BindingResult bindingResult = webDataBinder.getBindingResult();
         if(bindingResult.hasErrors()){
             bindingResult.getAllErrors().forEach(objectError -> log.debug(objectError.toString()));
             return RECIPE_RECIPEFORM_URL;
         }
 
-        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command).block();
-
-        return "redirect:/recipe/" + savedCommand.getId() + "/show";
+        recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/" + command.getId() + "/show";
     }
 
     @GetMapping("recipe/{id}/delete")
