bdfec32b1e0d1d0403e124ba1696baf74a0e3167
Shumyk <dima.shumyk@gmail.com>
2020-05-26 16:18:18 +0300

    [355] Added WebFlux exception handling for NotFoundException and TemplateInputException

--- src/main/java/com/shumyk/controllers/RecipeController.java
+++ src/main/java/com/shumyk/controllers/RecipeController.java
@@ -1,15 +1,18 @@
 package com.shumyk.controllers;
 
 import com.shumyk.commands.RecipeCommand;
+import com.shumyk.exceptions.NotFoundException;
 import com.shumyk.services.RecipeService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 @Slf4j
 @Controller
@@ -67,19 +70,15 @@ public class RecipeController {
         return "redirect:/";
     }
 
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(NotFoundException.class)
-//    public ModelAndView handleNotFound(Exception exception){
-//
-//        log.error("Handling not found exception");
-//        log.error(exception.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//
-//        modelAndView.setViewName("404error");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+    public String handleNotFound(Exception exception, Model model) {
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        model.addAttribute("exception", exception);
+
+        return "404Error";
+    }
 
 }
