12e40a1af71710f0f61ceafc3dcffda7382873df
Shumyk <dima.shumyk@gmail.com>
2020-05-26 15:21:06 +0300

    [353] Added binding validation for ingredient.

--- src/main/java/com/shumyk/controllers/IngredientController.java
+++ src/main/java/com/shumyk/controllers/IngredientController.java
@@ -6,26 +6,30 @@ import com.shumyk.commands.UnitOfMeasureCommand;
 import com.shumyk.services.IngredientService;
 import com.shumyk.services.RecipeService;
 import com.shumyk.services.UnitOfMeasureService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
-/**
- * Created by jt on 6/28/17.
- */
+import javax.validation.constraints.NotNull;
+
 @Slf4j
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class IngredientController {
 
-    private final IngredientService ingredientService;
-    private final RecipeService recipeService;
-    private final UnitOfMeasureService unitOfMeasureService;
+    @NotNull private final IngredientService ingredientService;
+    @NotNull private final RecipeService recipeService;
+    @NotNull private final UnitOfMeasureService unitOfMeasureService;
+
+    private WebDataBinder webDataBinder;
+
+    @InitBinder("ingredient") public void initBinder(final WebDataBinder webDataBinder) {
+        this.webDataBinder = webDataBinder;
+    }
 
     @GetMapping("/recipe/{recipeId}/ingredients")
     public String listIngredients(@PathVariable String recipeId, Model model){
@@ -72,12 +76,19 @@ public class IngredientController {
     }
 
     @PostMapping("recipe/{recipeId}/ingredient")
-    public String saveOrUpdate(@ModelAttribute IngredientCommand command){
+    public String saveOrUpdate(@ModelAttribute("ingredient") IngredientCommand command, @PathVariable String recipeId, Model model) {
+        webDataBinder.validate();
+        final BindingResult bindingResult = webDataBinder.getBindingResult();
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(error -> log.error(error.toString()));
+            model.addAttribute("uomList", unitOfMeasureService.listAllUoms());
+            return "/recipe/ingredient/ingredientform";
+        }
+
+        command.setRecipeId(recipeId);
         IngredientCommand savedCommand = ingredientService.saveIngredientCommand(command).block();
-
         log.debug("saved ingredient id:" + savedCommand.getId());
-
-        return "redirect:/recipe/" + savedCommand.getRecipeId() + "/ingredient/" + savedCommand.getId() + "/show";
+        return "redirect:/recipe/" + recipeId + "/ingredient/" + savedCommand.getId() + "/show";
     }
 
     @GetMapping("recipe/{recipeId}/ingredient/{id}/delete")
